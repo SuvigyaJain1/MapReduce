c3db458c0214c959854e6c2358f1ed21d85ba50b
SuvigyaJain1 <suvigyajain1@gmail.com>
2022-10-31 20:26:14 +0530

    master skeleton

--- src/coordinator.py
+++ src/coordinator.py
@@ -1,3 +1,4 @@
+from asyncio import subprocess
 import json
 from flask import Flask, request
 import datetime
@@ -42,12 +43,42 @@ class Coordinator:
 
     def partition_input_file(self, file):
         """
-        Split the input text file into num_worker partitions.
-        Store partitions in tmp/task_name/
+        Split the input text file into num_worker splits. Each split corresponds to a partition
+        Store splits in tmp/task_name/<partition#>
         Returns a the partition directory
         """
         pass
+    
+    def assign_partitions_to_workers(self, partitions_dir):
+        """
+        Use any round-robin/ hash style partitioning to assign each partition to any 1 worker
+        """
+        pass
+    
+    def start_map(self):
+        """
+        Sends commands to workers to start the map tasks on their respective partitions. 
+        1 worker may get more than 1 command (1 per partition assigned to it)
+        """
+        pass
 
+    def await_map_results(self, callback):
+        """
+        start a process that checks every x seconds if a map process has ended. 
+        when all maps end, collect intermediate outputs and shuffle them to correct partitions
+        based on hash/range. Then execute callback (indicate to workers to start reduce phase)
+        """
+        pass
+
+    def start_task(self):
+        """
+        Wrapper around await_map_results, await_reduce_results and await_task to make it easier to run them asynchronously.
+        """
+        self.start_map()
+        self.await_map_results()
+        self.await_reduce_results()
+        self.end_task()
+    
     def start_server(self):
 
         # ====================ROUTES==================
@@ -67,18 +98,26 @@ class Coordinator:
 
         @self.app.route("/schedule", methods=["POST"]):
         def shched_mapred_task():
+            if self.cur_task != "IDLE":
+                return "Please wait for previous task to finish before scheduling new task"
+            
             data = json.loads(request.data)
 
             assert "input_file" in data
             assert "task_name" in data
 
+            self.cur_task = data["task_name"]
+            self.probe_workers()
             partitions_dir = self.partition_input_file(data["input_file"])
-            
-            pass
+            self.assign_partitions_to_workers(partitions_dir)
+            self.start_task()
+
+            return "task scheduled successfully"
 
         @self.app.route("/report/<phase>/<worker>/<status>")
         def report_map_status(phase, worker, status):
             """
+            end point for worker to report status of task to coordinator
             @params:
                 phase
                     value must be one of map or reduce
